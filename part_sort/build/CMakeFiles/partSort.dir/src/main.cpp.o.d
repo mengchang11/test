0a22d9ab2cbcb0ffa7f6080bc0b231c497b674a9
mengc0508 <mencghangpc@outlook.com>
2023-01-30 23:43:57 +0800

    删除多余文件

--- part_sort/build/CMakeFiles/partSort.dir/src/main.cpp.o.d
+++
@@ -1,5 +0,0 @@
-CMakeFiles/partSort.dir/src/main.cpp.o: \
- /home/mengchang0508/github/test/test/part_sort/src/main.cpp \
- /usr/include/stdc-predef.h \
- /home/mengchang0508/github/test/test/part_sort/inc/print.h \
- /home/mengchang0508/github/test/test/part_sort/inc/part_sort.h
